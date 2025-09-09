fd4ef0c1adf36fd059aa7e3197ca6a54225f505f
Keen2Kode <70382344+Keen2Kode@users.noreply.github.com>
2025-09-10 00:54:21 +1000

    Time: 2 ms (78.69%), Space: 47.6 MB (38.8%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cs
@@ -0,0 +1,39 @@
+public class Solution {
+
+    // solution discoveries for 1st problem here
+    // https://github.com/Keen2Kode/LeetCode-Solutions/blob/main/0078-subsets/0078-subsets.cs
+
+    private List<IList<int>> subsets = new List<IList<int>>();
+    private int[] nums;
+    public IList<IList<int>> SubsetsWithDup(int[] nums) {
+
+        // presort to allow skipping duplicates easily in recursive method.
+        // O(n log(n)) is less than O(n * 2^n), so safe to sort
+        this.nums = nums;
+        Array.Sort(this.nums);
+
+        SubsetTree(new List<int>(), 0);
+        // foreach (List<int> subset in subsets) {
+        //     Console.WriteLine(String.Join("->", subset));
+        // }
+        return subsets;
+    }
+
+    private void SubsetTree(List<int> subset, int i) {
+        if (i==nums.Length) {
+            subsets.Add(new List<int>(subset));
+            return;
+        }
+        SubsetTree(new List<int>(subset) { nums[i] },i+1);
+        SubsetTree(subset, SkipDuplicates(i));
+    }
+
+    // the duplicate will already have been added above
+    // skip the entire branch for adding the 2nd duplicate 
+    private int SkipDuplicates(int i) {
+        int next = i+1;
+        while (next < nums.Length && nums[next] == nums[i])
+            next++;
+        return next;
+    }
+}
