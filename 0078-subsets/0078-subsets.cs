03cbc5a0647866f23eecebab56ac47933fd8a0e7
Keen2Kode <70382344+Keen2Kode@users.noreply.github.com>
2025-09-09 21:45:39 +1000

    Time: 0 ms (100%), Space: 47.1 MB (36.82%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cs
@@ -0,0 +1,31 @@
+public class Solution {
+
+    private List<IList<int>> subsets = new List<IList<int>>();
+    private int[] nums;
+    public IList<IList<int>> Subsets(int[] nums) {
+        // a binary tree branching for every element in nums, either choose it or don't
+        // the final leaf contains the subset to add
+        // no need to go back, so skipping a node class
+        this.nums = nums;
+
+        SubsetTree(new List<int>(), 0);
+        // foreach (List<int> subset in subsets) {
+        //     Console.WriteLine(String.Join("->", subset));
+        // }
+        return subsets;
+    }
+
+    // nums[i] is the current element to choose
+    private void SubsetTree(List<int> subset, int i) {
+        if (i==nums.Length) {
+            subsets.Add(subset);
+            return;
+        }
+
+        subset = new List<int>(subset);
+        SubsetTree(subset,i+1);
+        subset = new List<int>(subset);
+        subset.Add(nums[i]);
+        SubsetTree(subset,i+1);
+    }
+}
