dabe0dc131f2ff68ba6fc5917f5efd5299d163ca
Keen2Kode <70382344+Keen2Kode@users.noreply.github.com>
2025-09-11 01:07:54 +1000

    Time: 1 ms (85%), Space: 47.8 MB (47.61%) - LeetHub

--- 0046-permutations/0046-permutations.cs
+++ 0046-permutations/0046-permutations.cs
@@ -1,34 +1,20 @@
 public class Solution {
-    private List<IList<int>> permutations = new List<IList<int>>();
-    private int[] nums;
     public IList<IList<int>> Permute(int[] nums) {
-        this.nums = nums;
-        NaryTree(new List<int>(), new HashSet<int>());
-        return permutations;
+        IList<IList<int>> result = new List<IList<int>>();
+        Backtrack(nums, new List<int>(), result);
+        return result;
     }
 
-    private void NaryTree(List<int> permutation, HashSet<int> removed) {
-        bool removedAll = removed.Count == nums.Length;
-        if (removedAll) {
-            permutations.Add(new List<int>(permutation));
+    private void Backtrack(int[] nums, List<int> path, IList<IList<int>> result) {
+        if (path.Count == nums.Length) {
+            result.Add(new List<int>(path));
             return;
         }
-
-        // choosing an element means it can never be used in that perm again
-        // so it must be removed for next time.
-        // Removing from nums causes ConcurrentModificationException
-        // so why not ADD to a different list
         foreach (int num in nums) {
-            if (!removed.Contains(num)) {
-                removed.Add(num);
-                permutation.Add(num);
-                NaryTree(permutation, removed);
-                permutation.RemoveAt(permutation.Count-1);
-                removed.Remove(num);
-            }
-
+            if (path.Contains(num)) continue;
+            path.Add(num);
+            Backtrack(nums, path, result);
+            path.RemoveAt(path.Count - 1);
         }
-
-
     }
 }
