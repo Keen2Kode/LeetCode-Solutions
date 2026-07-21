151c491c793999f108bcfd7c283f227408a04785
Keen2Kode <70382344+Keen2Kode@users.noreply.github.com>
2026-07-21 15:49:26 +1000

    Time: 12 ms (19.05%), Space: 55.9 MB (9.52%) - LeetHub

---
+++ 0937-reorder-data-in-log-files/0937-reorder-data-in-log-files.cs
@@ -0,0 +1,52 @@
+public class Solution {
+    public string[] ReorderLogFiles(string[] logs) {
+        // "dig1 8 1 5 1",
+        // "let1 art can"
+        // "dig2 3 6"
+        // "let2 own kit dig"
+        // "let3 art zero"
+        // group digit and letter logs
+        // unclear identifier, use 2nd word
+
+        // build list of letter and digit logs
+        List<string> letterLogs = new();
+        List<string> digitLogs = new();
+        foreach (string log in logs) {
+            string word2 = log.Split(" ")[1];
+            if (IsLetterLog(word2)) {
+                letterLogs.Add(log);
+            } else {
+                digitLogs.Add(log);
+            }
+        }
+
+        letterLogs.Sort((a,b) => CompareLetterLogs(a,b));
+        List<string> finalLogs = letterLogs.Concat(digitLogs).ToList();
+
+        return finalLogs.ToArray();
+        
+    }
+    private static int CompareLetterLogs(string a, string b) {
+        Console.WriteLine($"Comparing a: {a}, to b: {b}");
+        string[] arrayA = a.Split(" ", 2);
+        string[] arrayB = b.Split(" ", 2);
+        
+        string idA = arrayA[0];
+        string idB = arrayB[0];
+        string contentA = arrayA[1];
+        string contentB = arrayB[1];
+
+        if (contentA.CompareTo(contentB) != 0) 
+            return contentA.CompareTo(contentB);
+        return idA.CompareTo(idB);
+
+    }
+
+    private static bool IsLetterLog(string word2) {
+        foreach (char c in word2) {
+            if (char.IsLower(c))
+                return true;
+        }
+        return false;
+    }
+}
